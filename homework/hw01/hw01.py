eb1a84aafb1a0eebfadad4c9a5fd2ed495306bed
Ahmed Ali Mohamed <ahmadalymohamad@gmail.com>
2020-06-15 10:56:01 +0200

    solution for hw01 control

--- homework/hw01/hw01.py
+++ homework/hw01/hw01.py
@@ -15,11 +15,11 @@ def a_plus_abs_b(a, b):
     ['return h(a, b)']
     """
     if b >= 0:
-        h = _____
+        h = add
     else:
-        h = _____
+        h = sub
     return h(a, b)
-
+# print(a_plus_abs_b(2, 3))
 def two_of_three(x, y, z):
     """Return a*a + b*b, where a and b are the two smallest members of the
     positive numbers x, y, and z.
@@ -38,8 +38,31 @@ def two_of_three(x, y, z):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(two_of_three)).body[0].body]
     ['Expr', 'Return']
     """
-    return _____
-
+    a = 0
+    b = 0
+    if x <= y and y <= z :
+        a = x
+        b = y
+    elif x <= y and z <= y:
+        a = x
+        b = z
+    elif y <= x and x <= z:
+        a = y
+        b = x
+    elif y <= x and z <= x:
+        a = y
+        b = z
+    elif z <= x and x <= y:
+        a = z
+        b = x
+    elif x <= z and z <= y:
+        a = x
+        b = z
+    return add(a*a, b*b)
+# print(two_of_three(1, 2, 3))
+# print(two_of_three(5, 3, 1))
+# print(two_of_three(10, 2, 8))
+# print(two_of_three(5, 5, 5))
 def largest_factor(x):
     """Return the largest factor of x that is smaller than x.
 
@@ -50,7 +73,15 @@ def largest_factor(x):
     >>> largest_factor(13) # factor is 1 since 13 is prime
     1
     """
-    "*** YOUR CODE HERE ***"
+    if x%2 == 0:
+        return x/2
+    elif x%3 == 0:
+        return x/3
+    elif x%5 == 0:
+        return x/5
+    else:
+        return 1
+
 
 def if_function(condition, true_result, false_result):
     """Return true_result if condition is a true value, and
@@ -66,9 +97,9 @@ def if_function(condition, true_result, false_result):
     5
     """
     if condition:
-        return true_result
+        return print(true_result)
     else:
-        return false_result
+        return print(false_result)
 
 
 def with_if_statement():
@@ -94,13 +125,15 @@ def with_if_function():
     return if_function(c(), t(), f())
 
 def c():
-    "*** YOUR CODE HERE ***"
+    return True
 
 def t():
-    "*** YOUR CODE HERE ***"
+    return 5 , 6
 
 def f():
-    "*** YOUR CODE HERE ***"
+    return 5
+result = with_if_function()
+print(result)
 
 def hailstone(x):
     """Print the hailstone sequence starting at x and return its
@@ -117,4 +150,18 @@ def hailstone(x):
     >>> a
     7
     """
-    "*** YOUR CODE HERE ***"
+    #record the length of the sequence
+    seq_length = 0
+    #loop through all x untill it is equal to x 
+    #print the current x in seqence and increase sequence length by 1
+    while x >= 1:
+        seq_length += 1
+        #print the current x and update it value to its half is it's even
+        if x%2 ==0 or x ==1:
+            print(x)
+            x = x/2
+        #if current value is odd, print it and use formula to update it
+        else:
+            print(x)
+            x = 3*x + 1
+    return seq_length
